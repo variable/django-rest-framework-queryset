7f033f14be5cc4a3eaaa7d14297cc1e9828f0409
James Lin <james.lin@grabone.co.nz>
2016-09-19 09:32:09 +1200

    bug fix

--- rest_framework_queryset/__init__.py
+++ rest_framework_queryset/__init__.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from __future__ import absolute_import
+
 __version__ = '0.0.9'
 
-from queryset import RestFrameworkQuerySet
+from .queryset import RestFrameworkQuerySet
