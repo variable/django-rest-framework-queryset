5f86924291923a85e31b96d48a56787161ec0c27
James Lin <jlin@Jamess-MacBook-Pro-2.local>
2019-05-01 16:32:23 +1200

    refactored HybridPagination

--- rest_framework_queryset/pagination.py
+++ rest_framework_queryset/pagination.py
@@ -3,34 +3,31 @@ from __future__ import unicode_literals
 from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 
 
-class HybridPagination(PageNumberPagination, LimitOffsetPagination):
+class HybridPagination(PageNumberPagination):
     """
     Basically allows both pagination method to work within a single pagination class.
     By default it uses the PageNumberPagination
     When 'offset' is used in request.GET, it will switch to use LimitOffsetPagination
     """
-    proxy = PageNumberPagination
+    page_size = 2
+    default_limit = 1
+
+    def __init__(self, *args, **kwargs):
+        self.proxy = None
+        return super(HybridPagination, self).__init__(*args, **kwargs)
 
     def paginate_queryset(self, queryset, request, view=None):
         if 'offset' in request.GET or 'limit' in request.GET:
-            self.proxy = LimitOffsetPagination
-        return self.proxy.paginate_queryset(self, queryset, request, view)
+            self.proxy = LimitOffsetPagination()
+            return self.proxy.paginate_queryset(queryset, request, view)
+        return super(HybridPagination, self).paginate_queryset(queryset, request, view)
 
-    def get_paginated_response(self, *args, **kwargs):
-        return self.proxy.get_paginated_response(self, *args, **kwargs)
-
-    def to_html(self):
-        return self.proxy.to_html(self)
-
-    def get_results(self, *args, **kwargs):
-        return self.proxy.get_results(self, *args, **kwargs)
-
-    def get_next_link(self, *args, **kwargs):
-        return self.proxy.get_next_link(self, *args, **kwargs)
-
-    def get_previous_link(self, *args, **kwargs):
-        return self.proxy.get_previous_link(self, *args, **kwargs)
-
-    def get_html_context(self, *args, **kwargs):
-        return self.proxy.get_html_context(self, *args, **kwargs)
+    def __getattribute__(self, item):
+        if item in ['paginate_queryset']:
+            return object.__getattribute__(self, item)
+        try:
+            proxy = object.__getattribute__(self, "proxy")
+            return object.__getattribute__(proxy, item)
+        except AttributeError:
+            return object.__getattribute__(self, item)
 
